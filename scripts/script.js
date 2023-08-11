51bed6d18a7f651fe0f6627e53a5c212effa4355
JonyPylon <jony@pylondata.com>
2023-08-11 20:04:08 +0200

    duck movement

--- scripts/script.js
+++ scripts/script.js
@@ -2,11 +2,29 @@
 const duck = document.querySelector('.duck');
 const gameContainer = document.querySelector('.game-container');
 
+let posX = Math.random() * (gameContainer.clientWidth - duck.clientWidth);
+let posY = Math.random() * (gameContainer.clientHeight - duck.clientHeight);
+let velocityX = (Math.random() - 0.5) * 7; // Velocity X random between -1 y 1
+let velocityY = (Math.random() - 0.5) * 7; // Velocity Y random between -1 y 1
+
 function moveDuck() {
-    const randomX = Math.random() * (gameContainer.clientWidth - duck.clientWidth);
-    const randomY = Math.random() * (gameContainer.clientHeight - duck.clientHeight);
-    duck.style.left = randomX + 'px';
-    duck.style.top = randomY + 'px';
+    posX += velocityX;
+    posY += velocityY;
+
+    // Limit the duck inside the container
+    if (posX < 0 || posX > gameContainer.clientWidth - duck.clientWidth) {
+        velocityX *= -1; // Change X direction when reaches the container border
+    }
+    if (posY < 0 || posY > gameContainer.clientHeight - duck.clientHeight) {
+        velocityY *= -1; // Change Y direction when reaches the container border
+    }
+
+    // Update duck positioning
+    duck.style.left = posX + 'px';
+    duck.style.top = posY + 'px';
+
+    requestAnimationFrame(moveDuck);
 }
 
-setInterval(moveDuck, 2000); // Cambiar la posición cada 2 segundos
+moveDuck(); // Starts animation
+
