c4e1116015b717d822b4a5888414f77fea485be7
JonyPylon <jony@pylondata.com>
2023-08-11 20:58:16 +0200

    play button animation

--- scripts/landing-script.js
+++ scripts/landing-script.js
@@ -1,5 +1,7 @@
+// Call game page when play button is selected
 const startGameButton = document.getElementById('start-game-button');
 
 startGameButton.addEventListener('click', () => {
     window.location.href = 'game.html'; 
 });
+
