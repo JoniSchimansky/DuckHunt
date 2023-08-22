2551bb557ba4168918f9f6d05c395f218662b86e
Josué Miró <josuemiro@gmail.com>
2023-08-22 17:52:57 +0200

    waves created, ducks management

---
+++ app/model/Duck.interface.ts
@@ -0,0 +1,18 @@
+export const DEFAULT_X_VELOCITY:number = 2;
+export const DEFAULT_Y_VELOCITY:number = 2;
+export const DEFAULT_WIDTH: number = 100;
+export const DEFAULT_HEIGHT: number = 70;
+export const DEFAULT_SCORE: number = 1;
+export const FRAME_INTERVAL: number = 10;
+export const VERTICAL_FALL: number = 7;
+
+export interface DuckInterface {
+    id: string;
+    xPosition: number;
+    yPosition: number;
+    xVelocity: number;
+    yVelocity: number;
+    isAlive: boolean;
+    isFlying: boolean;
+    defaultScore: number;
+}
